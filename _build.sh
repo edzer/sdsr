34c6775c2246878734091263cd3aff2ddd8886e3
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2022-10-20 15:48:33 +0200

    update

--- _build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = bookdown::gitbook(pandoc_args = c('--syntax-definition=r.xml', '--include-in-header=hide_code.html'))); warnings()"
-#Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::gitbook')
